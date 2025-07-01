ab45e8fed4698d4cce0fb5c5347e3ad42d4c8546
Yanghyunnmin <y86054361@gmail.com>
2025-07-02 05:02:07 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/likelion13thminihackathon/LikeLion13thMiniHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion13thminihackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLion13thMiniHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLion13thMiniHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelion13thminihackathon/LikeLion13thMiniHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion13thminihackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLion13thMiniHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
