a6b2a00da9afa4e449a16db71040992fa3d37b40
현지 <new5298@naver.com>
2025-07-05 00:22:09 +0900

    feat: 플래너 수정

--- src/main/java/com/example/likelion13thminihackathon/planner/PlannerDTO.java
+++ src/main/java/com/example/likelion13thminihackathon/planner/PlannerDTO.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class PlannerDTO {
-    private Long userId;
     private String semester;
     private Category category;  // Enum 타입
     private String goal;
@@ -16,3 +15,4 @@ public class PlannerDTO {
     private LocalDateTime updatedAt;
     private Boolean deletedAt;
 }
+
